247dc1ce1232466eccb0d12ec4e010264a5e51fe
SorokoletovDmitry <159958451+SorokoletovDmitry@users.noreply.github.com>
2024-05-03 17:08:37 +0300

    Add files via upload

--- keyboards/anketa.py
+++ keyboards/anketa.py
@@ -10,13 +10,24 @@ kb_apteka_cancel_and_back = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(
             text='Назад',
-            callback_data='back_anketa')
+            callback_data='back_anketa'),
         InlineKeyboardButton(
             text='Отмена',
             callback_data='cancel_anketa')]])
 
 kb_apteka_by_gender = InlineKeyboardMarkup(inline_keyboard=[
     [
-
-    ]
-])
+        InlineKeyboardButton(
+            text='Назад',
+            callback_data='back_anketa'),
+        InlineKeyboardButton(
+            text='Отмена',
+            callback_data='cancel_anketa')
+    ],[
+        InlineKeyboardButton(
+            text='Мужской',
+            callback_data='gender_m'),
+        InlineKeyboardButton(
+            text='Женский',
+            callback_data='gender_w')
+    ]])
